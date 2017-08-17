a5338853455876c0fe506435e9cf642f197e72a3
asmln <a.smln@yandex.ru>
2017-08-17 21:11:24 +0300

    Add request classes

---
+++ src/main/java/com/sbrw/auth/data/in/AuthReq.java
@@ -0,0 +1,27 @@
+package com.sbrw.auth.data.in;
+
+/**
+ * Auth
+ */
+public class AuthReq {
+
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
---
+++ src/main/java/com/sbrw/auth/data/in/TokenReq.java
@@ -0,0 +1,18 @@
+package com.sbrw.auth.data.in;
+
+/**
+ * Token
+ */
+public class TokenReq {
+
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+}
